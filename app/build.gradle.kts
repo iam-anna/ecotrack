b1fc71fdda9b65bf97762e13dc402a8e0da5c7bf
Anna <annaczr23@gmail.com>
2026-03-15 08:19:52 -0300

    merge conflicts correction

--- app/build.gradle.kts
+++ app/build.gradle.kts
@@ -58,4 +58,6 @@ dependencies {
     debugImplementation(libs.androidx.ui.test.manifest)
     implementation("androidx.lifecycle:lifecycle-viewmodel-compose:2.7.0")
     implementation("androidx.navigation:navigation-compose:2.7.7")
+    implementation ("com.squareup.retrofit2:retrofit:2.9.0")
+    implementation ("com.squareup.retrofit2:converter-gson:2.9.0")
 }
